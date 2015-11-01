43023fa5c866988239d6baeca470202a173d22d1
Sean <sean.thambiah@gmail.com>
2015-11-01 11:00:31 +0000

    Split factories into their own separate files

---
+++ js/transitions.js
@@ -0,0 +1,24 @@
+app.factory('transitions', [function(){
+    var o = {
+        gif: [
+            {url: 'http://i.giphy.com/slrRZ7bkPy6oU.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/pwE3XEUZdAJSE.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/od22gJJ5IdwFq.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/FhXFXgOjLnAS4.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/twIv2W4ZZxvP2.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/W62Cg8rUyhRwk.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/yugSj8GSC0wXm.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/q9qcnr2bxEWM8.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/KHF6bCDkreXeM.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/KYOD96tSm8Ovm.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/MYHKbrINiwOrK.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/10BbmRxI3vEvYI.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/WFGW9xik06wtG.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/8AireQ5HNdLYQ.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/Ns0Wq8qAA3aI8.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/lhN4K3aHjcucM.gif', playerStatus: "NOT PLAYING"},
+            {url: 'http://i.giphy.com/aARIdLTrIgU48.gif', playerStatus: "NOT PLAYING"}
+            
+    ]};
+    return o;
+}]);
